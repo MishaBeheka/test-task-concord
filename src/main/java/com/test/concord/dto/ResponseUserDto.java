4a9a14fa24a02adb769f7a859ab049190ec6d47d
Misha <mishabeheka@gmail.com>
2021-09-29 15:19:10 +0300

    Created model User and DTOs

---
+++ src/main/java/com/test/concord/dto/ResponseUserDto.java
@@ -0,0 +1,13 @@
+package com.test.concord.dto;
+
+public class ResponseUserDto {
+    private String fio;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+}
---
+++ src/main/java/com/test/concord/model/User.java
@@ -0,0 +1,14 @@
+package com.test.concord.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name")
+    private String fullName;
+}
